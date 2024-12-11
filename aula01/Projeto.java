d6762286337eb5a9c07368b5af464e987e4e491d
Marilia Brito de Carvalho <mariliabritodecarvalho@hotmail.com>
2024-12-11 20:09:51 -0300

    1º commit

---
+++ aula01/Projeto.java
@@ -0,0 +1,7 @@
+public class Projeto {
+    public static void main(String[] args) {
+        String nome = "Aula de Backend 1";
+        System.out.println("Olá mundo!");
+        System.out.println(nome);
+    }
+}
